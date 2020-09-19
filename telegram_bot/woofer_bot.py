0bb81493ee06a2d39f04c0586dd88b49f0caad6a
Anton Melnikov <antokhamelnikov@yandex.ru>
2020-09-19 21:33:54 +0300

    Command "fact" added

--- telegram_bot/woofer_bot.py
+++ telegram_bot/woofer_bot.py
@@ -1,7 +1,11 @@
 from .telegrambotapi import TelegramBotAPI
 import requests
+import json
+import random
+import os
 
 dogApiUrl = 'https://dog.ceo/api/breeds/image/random'
+dogFactsFilePath = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'static/facts/dog_facts.json'))
 
 class WooferBot(TelegramBotAPI):
 
@@ -23,11 +27,20 @@ class WooferBot(TelegramBotAPI):
         resDict = res.json()
         if resDict['status'] == 'success':
             dogImageUrl = resDict['message']
-            telegramResponse = self.sendPhoto(chat_id, dogImageUrl)
+            telegramResponse = self.sendPhoto(chat_id, dogImageUrl, caption='Woof! Woof!')
             if not telegramResponse['ok']:
                 print('Error: {}. {}'.format(telegramResponse['error_code'], telegramResponse['description']))
 
         return telegramResponse
 
     def commandFact(self, chat_id):
-        pass
+        dog_facts = []
+        with open(dogFactsFilePath) as json_file:
+            data = json.load(json_file)
+            dog_facts = data['dog_facts']
+        random_fact = dog_facts[random.randint(1, 60)]['fact']
+        telegramResponse = self.sendMessage(chat_id, random_fact)
+        if not telegramResponse['ok']:
+            print('Error: {}. {}'.format(telegramResponse['error_code'], telegramResponse['description']))
+
+        return telegramResponse
